86d5e565a970a98900bd89125dcfb8e34abe66ba
baohuy1303 <huynhbaohuy130333@gmail.com>
2026-07-13 00:26:32 -0400

    1.2 const pointers, const correctness, and placement new

--- Chap_1/1-2.cpp
+++ Chap_1/1-2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <new>
 using namespace std;
 
 /*
@@ -169,6 +170,63 @@ int main(){
     std::cout << vector_matrix[1][2] << std::endl; //5
 
     // L10: Const and Pointers
+    int org = 10;
+    int* normal_ptr = &org;
+    const int* ptr_to_const = &org; // cant change value from ptr
+    int* const const_ptr = &org; // cant change ptr address
+    const int* const const_ptr_to_const = &org; // both of the 2 above
 
+    //illegal ops
+    /* *ptr_to_const = 20; 
+    const_ptr = &rows; */
+    //expression must be a modifiable lvalue
+
+    // L11: Breaking constness
+    // use const for memory and readability, always start with const unless we can prove that the var will change
+    int non_const_int = 1;
+    const int* ptr_to_non_const_int = &non_const_int;
+    int* change_ptr = const_cast<int*>(ptr_to_non_const_int);
+    *change_ptr = 2;
+    std::cout << non_const_int << std::endl;
+    std::cout << *change_ptr << std::endl;
+        // non-const int displays 2 for both, const int displays 1 and then 2
+        // use this for when a legacy func or api doesnt use const correctly or missing const, then we can const_cast
+
+    // L12: Placement New
+    // Instead of using just new ... (which allocates memo on heap and constructs object then returns pointer all at once)
+    // We can use placement new, which constructs on pre-allocated memory we've made elsewhere. 
+    // We can decide what we want to construct on that allocated memo
+    // Would also return a pointer, but we have much more control over how we want to mangage the space.
+    // Use cases: memo pool with offsets (faster than heap allocation new), reconstructing in place (avoid overhead of requesting and deallocating), custom allocators
+    char buffer[sizeof(int)];
+    int* placement_new_ptr = new (buffer) int(42);
+    std::cout << *placement_new_ptr << std::endl;
+    //placement_new_ptr -> ~int();
+
+    class MyClass
+    {
+    public:
+        MyClass(int x, double y){
+            std::cout << "Constructed" << std::endl;
+        };
+        ~MyClass(){
+            std::cout << "Destroyed" << std::endl;
+        };
+    };
+
+    alignas(MyClass) char class_buffer[sizeof(MyClass) * 2]; //alignas ensures that the buffer is byte-algined
+    MyClass* my_class_ptr1 = new (class_buffer) MyClass(1, 2);
+    MyClass* my_class_ptr2 = new (class_buffer + sizeof(MyClass)) MyClass(3, 4);
+
+    my_class_ptr2->~MyClass();
+    my_class_ptr1->~MyClass();
+    // In-place construction
+    MyClass* bonus = new MyClass(3, 5);
+    bonus->~MyClass(); // if we don't deconstruct, it will just overwrite the old object (which is bad depending on what the deconstruction does)
+    new (bonus) MyClass(2, 1); // this takes the old space left by the 1st object
+    delete bonus; // deletes 2nd object
+
+
+    // L13: 
 }
 
